c2ab52d5e0ce01f2d41b391806dc784240fa0ab2
Prakhar Nagore <prakharnagore111@gmail.com>
2024-06-30 23:48:09 +0530

    add test to exclude password

--- tests/users/user.spec.ts
+++ tests/users/user.spec.ts
@@ -72,5 +72,36 @@ describe("GET /auth/self", () => {
             // check if your id matches with registered user
             expect((response.body as Record<string, string>).id).toBe(data.id);
         });
+
+        it("should not return the password field", async () => {
+            // register user
+            const userData = {
+                firstName: "Prakhar",
+                lastName: "Nagore",
+                email: "prakharnagore000@gmail.com",
+                password: "secret01",
+            };
+
+            const userRepository = connection.getRepository(User);
+            const data = await userRepository.save({
+                ...userData,
+                role: Roles.CUSTOMER,
+            });
+            // renerate token (npm i mock-jwks)
+            const accessToken = jwks.token({
+                sub: String(data.id),
+                role: data.role,
+            });
+            // add token to cookie
+            const response = await request(app)
+                .get("/auth/self")
+                .set("Cookie", [`accessToken=${accessToken};`])
+                .send();
+            // assert
+            // check if your id matches with registered user
+            expect(response.body as Record<string, string>).not.toHaveProperty(
+                "password",
+            );
+        });
     });
 });
